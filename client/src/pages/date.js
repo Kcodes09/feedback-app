c1f93aa8bd551347d1a65425db8e98814b6da394
Kcodes09 <keshavgiridharan09@gmail.com>
2025-07-20 22:20:46 +0530

    updated

--- client/src/pages/date.js
+++
@@ -1,5 +0,0 @@
-const today = new Date();
-const shortDateUS = today.toLocaleDateString('en-US'); // Example: "7/20/2025"
-const shortDateUK = new Date().toLocaleDateString('en-IN'); // Example: "20/07/2025"
-console.log(shortDateUS);
-console.log(shortDateUK);
